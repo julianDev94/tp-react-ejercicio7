d8777f996cef3ccf69308ed97f031b30c665504e
JulianAballay <juliancabj1210@gmail.com>
2024-02-02 00:29:39 -0300

    agrego componente listgroup de react en componente empleadolist

--- src/components/EmpleadoList.jsx
+++ src/components/EmpleadoList.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const EmpleadoList = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <ListGroup>
+      
+    </ListGroup>
+  );
 };
 
+//<ListGroup.Item>Cras justo odio</ListGroup.Item>
 export default EmpleadoList;
