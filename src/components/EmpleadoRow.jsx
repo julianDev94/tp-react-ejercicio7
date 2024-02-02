7d43db0f4465e954cc722af934ac98bbb9290236
JulianAballay <juliancabj1210@gmail.com>
2024-02-02 11:00:00 -0300

    ordeno mejor el componente empleadoRow

--- src/components/EmpleadoRow.jsx
+++ src/components/EmpleadoRow.jsx
@@ -1,13 +1,14 @@
 import { ListGroup, Card } from "react-bootstrap";
 
-const EmpleadoRow = ({empleado}) => {
+const EmpleadoRow = ({ empleado }) => {
   return (
     <ListGroup.Item className="my-3 d-flex justify-content-center border-0">
-      <Card className="my-3 w-50">
-        <Card.Body>
-          <Card.Title className="text-center">{empleado.fullName}</Card.Title>
-          <Card.Text className="text-center">
-            {empleado.title} {empleado.department}
+      <Card className="my-3 w-50 text-center">
+        <Card.Title className="mt-3">{empleado.fullName}</Card.Title>
+        <Card.Body className="d-flex justify-content-center">
+          <Card.Text className="me-3">{empleado.title}</Card.Text>
+          <Card.Text className="text-bg-primary">
+            {empleado.department}
           </Card.Text>
         </Card.Body>
       </Card>
